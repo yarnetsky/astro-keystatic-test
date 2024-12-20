2ff8f962bc380a9368f2292963249c2a9b168f66
Jerry Yarnetsky <36451755+yarnetsky@users.noreply.github.com>
2024-12-20 22:43:40 +0000

    delete z-older

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,22 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import vercel from '@astrojs/vercel';
+import react from '@astrojs/react';
+import markdoc from '@astrojs/markdoc';
+import keystatic from '@keystatic/astro';
+
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+	integrations: [
+		react(), 
+		mdx(), 
+		markdoc(), 
+		keystatic(), 
+		sitemap()
+	],
 	output: 'server',
   	adapter: vercel(),
 });
